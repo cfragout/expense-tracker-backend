dfbf1e626a9cf0d1fae2fd77687b4a83daeaf800
cfragout <cfragout@outlook.com>
2022-09-20 12:57:39 -0300

    expenses by category. expenses by date from and to. fix amount being a string instead of number

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const expenses = [
         user: 'testy',
         currency: '2',
         description: '',
-        category: '1',
+        category: '3',
         date: '2022-09-19T21:34:39.163Z'
     },
     {
@@ -118,10 +118,74 @@ const expenses = [
         category: '1',
         date: '2022-09-02T20:34:39.163Z'
     },
+    {
+        id: '12',
+        amount: 30,
+        user: 'testy',
+        currency: '2',
+        description: '',
+        category: '3',
+        date: '2022-09-02T20:34:39.163Z'
+    },
+    {
+        id: '13',
+        amount: 12,
+        user: 'juan',
+        currency: '2',
+        description: '',
+        category: '4',
+        date: '2022-09-02T20:34:39.163Z'
+    },
+    {
+        id: '14',
+        amount: 12,
+        user: 'juan',
+        currency: '2',
+        description: '',
+        category: 'prueba',
+        date: '2022-09-02T20:34:39.163Z'
+    },
 ]
 
 app.get('/', (req, res) => {
     res.json(crypto.randomUUID())
+})
+
+app.get('/api/expenses/byCategory', (req, res) => {
+    // needs validations
+    const date = moment(req.query.date);
+    const limit = req.query.limit || 5;
+
+    const expensesInMonth = expenses.filter(e => {
+        const expenseDate = moment(e.date);
+        return expenseDate.isSame(date, 'month');
+    });
+
+    const expensesByCategory = {};
+    expensesInMonth.forEach(exp => {
+        if (expensesByCategory[exp.category]) {
+            expensesByCategory[exp.category] += exp.amount;
+        } else {
+            expensesByCategory[exp.category] = exp.amount;
+        }
+    })
+
+    res.json({ response: expensesByCategory })
+})
+
+app.get('/api/expenses', (req, res) => {
+    const from = moment(req.query.from);
+    const to = moment(req.query.to);
+
+    const expensesInRange = expenses.filter(e => {
+        const expenseDate = moment(e.date);
+        return expenseDate.isBetween(from, to, 'hour');
+    });
+
+
+    res.json({
+        response: expensesInRange
+    })
 })
 
 app.get('/api/expenses', (req, res) => {
@@ -140,12 +204,11 @@ app.get('/api/expenses/:id', (req, res) => {
     }
 })
 
+
 app.get('/api/expenses/monthly/:date', (req, res) => {
 
     // needs validations
     const date = moment(req.params.date);
-
-
     const lastDate = date.daysInMonth();
     const monthlyExpenses = [];
 
@@ -159,16 +222,43 @@ app.get('/api/expenses/monthly/:date', (req, res) => {
         });
 
         // need to check currency here
-        const totalExpenses = expensesForToday.length > 0 ? expensesForToday.map(e => parseInt(e.amount, 10)).reduce((prev, current) => prev + current) : 0;
+        const totalExpenses = expensesForToday.length > 0 ? expensesForToday.map(e => e.amount, 10).reduce((prev, current) => prev + current) : 0;
 
 
         monthlyExpenses.push(totalExpenses);
     }
 
     res.json({ response: monthlyExpenses });
-
-
 })
+
+
+// :date should be query string
+app.get('/api/expenses/monthly/:date', (req, res) => {
+
+    // needs validations
+    const date = moment(req.params.date);
+    const lastDate = date.daysInMonth();
+    const monthlyExpenses = [];
+
+    // may be able to do this better once mongodb is in place
+    for (let index = 1; index <= lastDate; index++) {
+
+        // all the expenses made in the date === index
+        const expensesForToday = expenses.filter(e => {
+            const expenseDate = moment(e.date);
+            return expenseDate.isSame(date, 'month') && expenseDate.date() === index;
+        });
+
+        // need to check currency here
+        const totalExpenses = expensesForToday.length > 0 ? expensesForToday.map(e => e.amount).reduce((prev, current) => prev + current) : 0;
+
+
+        monthlyExpenses.push(totalExpenses);
+    }
+
+    res.json({ response: monthlyExpenses });
+})
+
 
 app.post('/api/expenses', (req, res) => {
     const { amount, currency } = req.body;
@@ -185,10 +275,10 @@ app.post('/api/expenses', (req, res) => {
     }
 
     const newExpense = {
+        ...req.body,
         id: crypto.randomUUID(),
-        amount,
+        amount: parseInt(req.body.amount, 10),
         currency,
-        ...req.body
     }
 
     expenses.push(newExpense);
